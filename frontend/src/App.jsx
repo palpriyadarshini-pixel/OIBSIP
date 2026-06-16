dca97661b50059013d849b11f7251a619bfd3d13
Priyadarshini Pal <palpriyadarshini4@gmail.com>
2026-06-16 10:28:19 +0530

    Added Tailwind CSS setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/home";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Cart from "./pages/cart";
-import Orders from "./pages/orders";
-import Admin from "./pages/admin";
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/orders" element={<Orders />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-yellow-400">
+        Pizza Delivery
+      </h1>
+    </div>
   );
 }
 
